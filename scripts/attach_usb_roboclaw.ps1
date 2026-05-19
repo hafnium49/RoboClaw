49c67887af2e1f26a94d57b7f9d94381407b623d
hafnium49 <hafnium49@users.noreply.github.com>
2026-05-19 13:15:41 +0900

    chore: update default BusIds in attach_usb_roboclaw.ps1 script

--- scripts/attach_usb_roboclaw.ps1
+++ scripts/attach_usb_roboclaw.ps1
@@ -7,7 +7,7 @@
 
 param(
     [string]$Distro = "Ubuntu-roboclaw",
-    [string[]]$BusIds = @('4-3','4-4','5-1','5-3','5-4'),
+    [string[]]$BusIds = @('4-1','4-2','4-3','4-4','5-1','5-3','5-4'),
     [switch]$Detach
 )
 
